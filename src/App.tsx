19879a1c1880c8b69e37a8211b21f86127adbfbd
msrfjfj <mtsales1337@gmail.com>
2021-05-31 08:32:12 -0300

    primeiro commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Routes from './routes';
+import {BrowserRouter} from 'react-router-dom'
+
+
+const  App:React.FC = () => (
+  <BrowserRouter>
+  <Routes/>
+  </BrowserRouter>
+);
+
+
+export default App;
