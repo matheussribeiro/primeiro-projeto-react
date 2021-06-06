094975460838a41410c79331b5a9e0519cccb119
msrfjfj <mtsales1337@gmail.com>
2021-06-06 16:41:16 -0300

    Create Layout for Dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,8 +1,53 @@
 import React from 'react';
-import {Title} from './styles'
+import logoImg from '../../assets/githubexplores.svg'
+import {FiChevronRight} from 'react-icons/fi'
+
+
+import {Title, Form, Repositories} from './styles'
+
+
 
 const Dashboard: React.FC = () => {
-    return <Title>Explore repositórios no Github</Title>;
-}
+    return (
+    <>
+        <img src={logoImg} alt="Github Explorer"/>
+        <Title>Explore repositórios no Github</Title>
+
+        <Form>
+            <input placeholder="Digite o repositório"/>
+            <button type="submit">Pesquisar</button>
+        </Form>
+
+        <Repositories>
+            <a href="https://google.com.br">
+                <img src="https://avatars.githubusercontent.com/u/7717842?v=4" alt="Vitor Nere"/>
+                <div>
+                    <strong>rocketseat/unform</strong>
+                    <p>Descrição teste</p>
+                </div>
+                <FiChevronRight size={20}/> 
+            </a>
+
+            <a href="https://google.com.br">
+                <img src="https://avatars.githubusercontent.com/u/7717842?v=4" alt="Vitor Nere"/>
+                <div>
+                    <strong>rocketseat/unform</strong>
+                    <p>Descrição teste</p>
+                </div>
+                <FiChevronRight size={20}/> 
+            </a>
+
+            <a href="https://google.com.br">
+                <img src="https://avatars.githubusercontent.com/u/7717842?v=4" alt="Vitor Nere"/>
+                <div>
+                    <strong>rocketseat/unform</strong>
+                    <p>Descrição teste</p>
+                </div>
+                <FiChevronRight size={20}/> 
+            </a>
+        </Repositories>
+    </>
+    );
+};
 
 export default Dashboard;
