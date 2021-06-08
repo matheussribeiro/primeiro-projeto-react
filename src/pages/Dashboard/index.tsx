10e3ac98ccf2aafa2ad46bc937c6596b2891cb9a
msrfjfj <mtsales1337@gmail.com>
2021-06-07 22:38:40 -0300

    Conectando com a API

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,50 +1,58 @@
-import React from 'react';
-import logoImg from '../../assets/githubexplores.svg'
-import {FiChevronRight} from 'react-icons/fi'
-
-
+import React, {useState , FormEvent} from 'react';
+import logoImg from '../../assets/githubexplores.svg';
+import {FiChevronRight} from 'react-icons/fi';
+import api from '../../services/api';
 import {Title, Form, Repositories} from './styles'
+
+interface Repository{
+    full_name:string;
+    description:string;
+    owner:{
+        login:string;
+        avatar_url:string;
+    }
+}
 
 
 
 const Dashboard: React.FC = () => {
+    const [newRepo,setNewRepo] = useState('');
+    const [repositories,setRepositories] = useState<Repository[]>([]);
+
+    async function handleAddRepository(event : FormEvent<HTMLFormElement>):Promise<void>{
+        event.preventDefault()
+        console.log(newRepo)
+        const response = await api.get<Repository>(`repos/${newRepo}`);
+
+        const repository = response.data;
+
+        setRepositories([...repositories , repository]);
+        setNewRepo('');
+        
+    }
+    
     return (
     <>
         <img src={logoImg} alt="Github Explorer"/>
         <Title>Explore repositórios no Github</Title>
 
-        <Form>
-            <input placeholder="Digite o repositório"/>
+        <Form onSubmit={handleAddRepository}>
+            <input value={newRepo} onChange={(e) => setNewRepo(e.target.value)} placeholder="Digite o repositório"/>
             <button type="submit">Pesquisar</button>
         </Form>
 
         <Repositories>
-            <a href="https://google.com.br">
-                <img src="https://avatars.githubusercontent.com/u/7717842?v=4" alt="Vitor Nere"/>
+            {repositories.map(repository =>(
+                <a key={repository.full_name} href="teste">
+                <img src={repository.owner.avatar_url} alt={repository.owner.login}/>
                 <div>
-                    <strong>rocketseat/unform</strong>
-                    <p>Descrição teste</p>
+                    <strong>{repository.full_name}</strong>
+                    <p>{repository.description}</p>
                 </div>
                 <FiChevronRight size={20}/> 
             </a>
-
-            <a href="https://google.com.br">
-                <img src="https://avatars.githubusercontent.com/u/7717842?v=4" alt="Vitor Nere"/>
-                <div>
-                    <strong>rocketseat/unform</strong>
-                    <p>Descrição teste</p>
-                </div>
-                <FiChevronRight size={20}/> 
-            </a>
-
-            <a href="https://google.com.br">
-                <img src="https://avatars.githubusercontent.com/u/7717842?v=4" alt="Vitor Nere"/>
-                <div>
-                    <strong>rocketseat/unform</strong>
-                    <p>Descrição teste</p>
-                </div>
-                <FiChevronRight size={20}/> 
-            </a>
+            ))}
+            
         </Repositories>
     </>
     );
